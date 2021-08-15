bf4f7e39a678d38aedd3b0f6247fc1dcaccd6e1f
Shraddha Penshanwar <68414100+ShraddhaPenshanwar@users.noreply.github.com>
2021-08-15 17:07:20 +0530

    Add files via upload

---
+++ src/components/ContactList.js
@@ -0,0 +1,52 @@
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
+import ContactCard from "./ContactCard";
+
+const ContactList = (props) => {
+const inputEl = useRef("");
+
+  const deleteConactHandler = (id) => {
+    props.getContactId(id);
+  };
+
+  
+  const renderContactList = props.contacts.map((contact) => {
+    return (
+      <ContactCard
+        contact={contact}
+        clickHander={deleteConactHandler}
+        key={contact.id}
+      />
+    );
+  });
+
+  const getSearchTerm = () => {
+    props.searchKeyword(inputEl.current.value);
+  };
+  return (
+    <div class="main">
+      <h2>
+        Contact list
+      <Link to="/add">
+        <button className="ui button blue right">Add Contact</button>
+        </Link>
+      </h2>
+      <div className="ui search">
+        <div className="ui icon input">
+          <input
+          ref={inputEl}
+           type="text"
+            placeholder="Search Contacts" 
+            className="prompt" 
+            value={props.term} 
+            onChange={ getSearchTerm} />
+          <i className ="search icon"></i>
+        </div>
+
+      </div>
+  <div className="ui celled list">{renderContactList}</div>
+  </div>
+  );
+};
+
+export default ContactList;
